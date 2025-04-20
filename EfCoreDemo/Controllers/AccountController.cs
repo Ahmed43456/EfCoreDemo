9ae0eb00ee840f3ba7f57dbbd7be69430b5ea6fb
ahmad wz <ahmadwz482@gmail.com>
2025-04-19 20:09:36 -0700

    Add Account Controller and Register View Model

---
+++ EfCoreDemo/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using EfCoreDemo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EfCoreDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager; 
+        }
+        public async  Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                var result = await userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                   return RedirectToAction("Home", "Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return View(model);
+        }
+    }
+}
---
+++ EfCoreDemo/Models/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreDemo.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "كلمة المرور يجب أن تكون على الأقل 6 أحرف.")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "كلمتا المرور غير متطابقتين.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
